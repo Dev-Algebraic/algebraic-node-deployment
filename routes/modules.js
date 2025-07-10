f62a9deef7d9eac38310effb27ca51f8f69fccdc
Ishan Patel <87627473+HoloStudios@users.noreply.github.com>
2025-07-09 23:48:34 -0700

    Added route to get user score by module id request handler

--- routes/modules.js
+++ routes/modules.js
@@ -1,7 +1,7 @@
 import express, { response } from 'express';
 import bodyParser from 'body-parser';
 import { createModuleData, getAllModules } from '../helper/modules-helper.js';
-import { createQuizScore, getScoreData } from '../helper/userScore-helper.js';
+import { createQuizScore, getScoreData, getScoreDataByModuleId } from '../helper/userScore-helper.js';
 
 const app = express();
 const router = express.Router();
@@ -12,7 +12,8 @@ router.post("/create", createModuleData)
 router.get("/get/:userid", getAllModules) 
 
 router.post("/score/create", createQuizScore) 
-router.get("/score/:id", getScoreData) 
+router.get("/score/:id", getScoreData)
+router.get("/score/module/:id", getScoreDataByModuleId)
 
 
 export default router;
