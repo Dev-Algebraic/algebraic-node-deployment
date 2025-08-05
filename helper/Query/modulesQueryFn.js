c2edaf2eca6433282ff7dfac489530f25f1047b6
Ishan Patel <87627473+HoloStudios@users.noreply.github.com>
2025-08-05 19:37:19 -0400

    Removed module ordering in query

--- helper/Query/modulesQueryFn.js
+++ helper/Query/modulesQueryFn.js
@@ -50,7 +50,7 @@ const fetchCreatedModule = asyncHandler(async (id) => {
 
 const getModules = asyncHandler(async (userId,ParentId) => {
   return await db.query(
-    `select modules.id,modules.name,modules.description,order_no,user_score.score,user_score.quiz_attempt,user_score.total_questions from modules left join user_score on modules.id = user_score.module_fk and user_score.user_fk=? where modules.parent_id =?`,
+    `select modules.id,modules.name,modules.description,order_no,user_score.score,user_score.quiz_attempt,user_score.total_questions from modules left join user_score on modules.id = user_score.module_fk and user_score.user_fk=? where modules.parent_id =? ORDER BY modules.order_no ASC`,
 
     [userId,ParentId]
   );
