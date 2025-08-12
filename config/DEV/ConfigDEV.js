c212d01b914e222e3e18e24a6a09952102f06b1f
Ishan Patel <87627473+HoloStudios@users.noreply.github.com>
2025-08-12 12:46:13 -0400

    Patched exposed data

--- config/DEV/ConfigDEV.js
+++ config/DEV/ConfigDEV.js
@@ -1,12 +1,17 @@
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 const config = {
   db: { 
-    host: 'localhost',
-    user:  'AlgebraicD',
-    password: 'AlgebraicD1!',
-    port:  '5555',
-    database: 'algebraic_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port:  process.env.DB_PORT,
+    ssl: {
+      rejectUnauthorized: true
+    },
+    multipleStatements: true,
   },
 
 
